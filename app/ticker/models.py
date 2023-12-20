7b268c4e73250deba09fe2b4418e1284b5d8d740
Barabashka656 <alekseithebestofthebest@gmail.com>
2023-12-20 20:02:09 +0300

    added saving queries to the db

--- app/ticker/models.py
+++ app/ticker/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class Requests(models.Model):
+    name = models.CharField(max_length=255)
--- app/ticker/views.py
+++ app/ticker/views.py
@@ -2,6 +2,7 @@ import os
 
 from app.settings import BASE_DIR
 from ticker.ticker_init import create_ticker
+from ticker.models import Requests
 
 from django.conf import settings
 from django.http import FileResponse, Http404
@@ -10,12 +11,15 @@ from django.shortcuts import render, HttpResponse
 
 def index(request, text: str):
     video_path = os.path.join(BASE_DIR, 'ticker', 'data', 'ticker.mp4')
+    if not Requests.objects.filter(name=text).exists():
+        request_instance = Requests(name=text)
+        request_instance.save()
+
     create_ticker(text, video_path)
     if os.path.exists(video_path):
         f = open(video_path, 'rb')
         response = FileResponse(f)
         response['Content-Disposition'] = 'attachment; filename="ticker.mp4"'
-        
         return response
     
     return Http404("Video file not found")
