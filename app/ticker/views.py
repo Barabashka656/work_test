1fb657ccd4249056589f6c59aabaae532610b6da
Barabashka656 <alekseithebestofthebest@gmail.com>
2023-12-20 18:36:57 +0300

    download file

--- app/ticker/views.py
+++ app/ticker/views.py
@@ -3,11 +3,19 @@ import os
 from app.settings import BASE_DIR
 from ticker.ticker_init import create_ticker
 
-
+from django.conf import settings
+from django.http import FileResponse, Http404
 from django.shortcuts import render, HttpResponse
+
 
 def index(request, text: str):
     video_path = os.path.join(BASE_DIR, 'ticker', 'data', 'ticker.mp4')
-    
     create_ticker(text, video_path)
-    return HttpResponse('HELLO')
+    if os.path.exists(video_path):
+        f = open(video_path, 'rb')
+        response = FileResponse(f)
+        response['Content-Disposition'] = 'attachment; filename="ticker.mp4"'
+        
+        return response
+    
+    return Http404("Video file not found")
