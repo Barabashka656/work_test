867469de7617c26b504d6fcb82b7bb0ac23b5cd0
Barabashka656 <alekseithebestofthebest@gmail.com>
2023-12-25 16:49:41 +0300

    allow hosts

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3i@6%=8sh&=(rd)#b3zl!n-da9wk=!)i%6pt*w8$t2@-*he*xh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
